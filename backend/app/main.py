b5ba95eed302021bf49796cf9c837e5d7d870423
Ahmoham98 <electricallover45@gmail.com>
2025-02-19 12:37:18 +0330

    add models to the main file and create them with create_db_tables function called in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel
 from .views import user_views
+
+from .database import engine
+from .models import users, products, orders, order_items, categories
 
 app = FastAPI()
 
 app.include_router(user_views.router)
 
 
+def create_db_tables():
+    SQLModel.metadata.create_all(bind=engine)
+
 def main():
-    pass
+    create_db_tables()
 
 
 if __name__ == "__main__":
