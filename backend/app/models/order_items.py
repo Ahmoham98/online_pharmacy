1c53d19d1026872d7a1b9bb6f5d63a4668b9742a
Ahmoham98 <electricallover45@gmail.com>
2025-02-19 07:20:39 +0330

    order_items models

---
+++ backend/app/models/order_items.py
@@ -0,0 +1,54 @@
+from sqlmodel import SQLModel, Field
+
+# defining order_items base model
+
+class OrderItemsBase(SQLModel):
+    title: str
+    description: str
+    created_at: str = Field(default="now")
+    updated_at: str = Field(default="now")
+
+# define order_items table
+
+class OrderItems(OrderItemsBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    product_id: int | None = Field(default=None, foreign_key="products.id")
+    orders_id: int | None = Field(default=None, foreign_key="orders.id")
+    
+    uint_price: int
+    sale_price: int
+    amount: int
+    total_price: int
+    status: str
+    is_active: bool
+
+# define order_items input model
+
+class OrderItemsCreate(OrderItemsBase):
+    amount: int
+    status: str
+
+
+# define order_items output model
+
+class OrderItemsPublic(OrderItemsBase):
+    id: int
+    products_id: int
+    orders_id: int
+    amount: int
+    total_price: int
+    status: str
+
+# define order_items update model
+
+class OrderItemsUpdate(SQLModel):
+    title: str | None = None
+    description: str | None = None
+    unit_price: int | None = None
+    sale_price: int | None = None
+    is_active: bool | None = None
+    status: str | None = None
+    created_at: str | None = None
+    updated_at: str | None = None
+
+
