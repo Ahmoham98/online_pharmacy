98f2033c07eec214dda603cdd57c12d941ac7bc2
Ahmoham98 <electricallover45@gmail.com>
2025-02-20 13:13:01 +0330

    setting description for openapi documentation

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,72 @@ from .views import user_views, product_views, order_views, order_item_views, cat
 from .database import engine
 from .models import users, products, orders, order_items, categories
 
-app = FastAPI()
+description = """
+## Users
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+
+## products
+You will be able to:
+* **Create products** (_not implemented_).
+* **Read products** (_not implemented_).
+
+## orders
+You will be able to:
+* **Create orders** (_not implemented_).
+* **Read orders** (_not implemented_).
+
+## order_items
+You will be able to:
+* **Create order items** (_not implemented_).
+* **read order items** (_not implemented_.)
+
+## categories
+You will be able to:
+* **Create categories** (_not implemented_).
+* **Read categories** (_not implemented_).
+
+"""
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "users endpoints. **login** Logic also implemented in here"
+    },
+    {
+        "name": "products",
+        "description": "products endpoints. **getting** & **reading** the products"
+    },
+    {
+        "name": "orders",
+        "description": "orders endpoints. **getting** & **reading** orders"
+    },
+    {
+        "name": "order_items",
+        "description": "order_items endpoints. **getting** & **reading** order_items"
+    },
+    {
+        "name": "categories",
+        "description": "categories endpoints. **getting** & **reading** categories"
+    },
+    {
+        "name": "admin",
+        "description": "admin part. for uploading and different accesses type from normal user"
+    },
+    {
+        "name": "default",
+        "description": "**get** root to check the server functionality and some other case usage"
+    },
+]
+
+app = FastAPI(
+    title="Online Pharmacy API's",
+    description=description,
+    summary="Online Pharmacy: save time by getting requirements online...",
+    version="0.0.7",
+    openapi_tags=tags_metadata
+)
 
 app.include_router(user_views.router)
 app.include_router(product_views.router)
