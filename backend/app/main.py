94e9d56a8db5ef603dfdafcfdc9e3d7480201653
Ahmoham98 <electricallover45@gmail.com>
2025-02-20 11:41:26 +0330

    update routers in main package file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from sqlmodel import SQLModel
-from .views import user_views, product_views
+from .views import user_views, product_views, order_views, order_item_views, category_views
 
 from .database import engine
 from .models import users, products, orders, order_items, categories
@@ -9,6 +9,9 @@ app = FastAPI()
 
 app.include_router(user_views.router)
 app.include_router(product_views.router)
+app.include_router(order_views.router)
+app.include_router(order_item_views.router)
+app.include_router(category_views.router)
 
 def create_db_tables():
     SQLModel.metadata.create_all(bind=engine)
