832d7dc79a02b1f5139036bb25b77d9ed099c747
Ahmoham98 <electricallover45@gmail.com>
2025-02-22 10:11:48 +0330

    sending user controllers function to the user_controller file

---
+++ backend/app/controllers/user_controller.py
@@ -0,0 +1,80 @@
+from sqlmodel import Session, select
+from fastapi import Depends, HTTPException
+from ..schema.users_schema import UsersUpdate, UsersCreate
+from ..models.users import Users
+
+from ..dependency import get_session
+
+# get all of the user
+
+def get_users_controller( session: Session):
+    users = session.exec(select(Users)).all()
+    return users
+
+# post user
+
+def post_user_controller(session: Session, user: UsersCreate):
+    db_user = Users.model_validate(user)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+# get user with id
+
+def get_user_controller(session: Session, user_id: int):
+    db_user = session.get(Users, user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="user not found!")
+    return db_user
+
+# delete user with id
+
+def delete_user_controller(session: Session, user_id):
+    db_user = session.get(Users, user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="user with given id not found! ")
+    session.delete(db_user)
+    session.commit()
+    
+    return {"message": "user deleted successfully!"}  # return a message to the client
+
+# update user with username
+
+def update_user_controller(session: Session, user: UsersUpdate):
+    db_user = session.exec(select(Users).where(user.username == Users.username)).one()
+    if user.username is None:
+        raise HTTPException(status_code=405, detail="username field required")
+    elif user.username == "string":
+        raise HTTPException(status_code=405, detail="username field required")
+    else:
+        db_user.username = user.username
+    
+    if user.password is not None:
+        db_user.password = user.password
+    
+    if user.email is not None:
+        db_user.email = user.email
+    
+    if user.phone is not None:
+        db_user.phone = db_user.phone
+    
+    if user.first_name is not None:
+        db_user.first_name = user.first_name
+    
+    if user.last_name is not None:
+        db_user.last_name = user.last_name
+    
+    if user.address is not None:
+        db_user.address = user.address
+    
+    if user.role is not None:
+        db_user.role = user.role
+    
+    if user.created_at is not None:
+        db_user.created_at = user.created_at
+    
+    
+    session.add(db_user)
+    session.commit()
+    return {"massage": "success!"}
--- backend/app/views/user_views.py
+++ backend/app/views/user_views.py
@@ -5,99 +5,35 @@ from ..dependency import get_session
 from ..schema.users_schema import UsersBase, UsersCreate, UsersPublic, UsersUpdate
 from ..models.users import Users
 
+from ..controllers.user_controller import get_users_controller, post_user_controller , get_user_controller, delete_user_controller, update_user_controller
+
 router = APIRouter(
     prefix="/users",
     tags=["users"],)
 
 
 #Creating get request for /users with a response model of defined User class that we can make python understand it using List from typing builtin python function
+
 @router.get("/", response_model=list[UsersPublic])
-async def get_users(
-    *,
-    session: Session = Depends(get_session),
-):
-        users = session.exec(select(Users)).all()
-        return users
+async def get_users(*, session: Session = Depends(get_session)):
+        return get_users_controller(session)
 
 #Creating a post request endpoint to /users
+
 @router.post("/", response_model=UsersPublic)
-async def create_user(
-    *,
-    session: Session = Depends(get_session),
-    user : UsersCreate
-):
-    db_user = Users.model_validate(user)
-    session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
-    return db_user
+async def create_user(*, session: Session = Depends(get_session), user : UsersCreate):
+    return post_user_controller(session, user)
 
 #Creating get request endpoint with sending parameters to /users with /users/{id}
+
 @router.get("/{user_id}/")
-async def get_user(
-    *,
-    session: Session = Depends(get_session),
-    user_id: int
-):
-    db_user = session.get(Users, user_id)
-    if not db_user:
-        raise HTTPException(status_code=404, detail="user not found!")
-    return db_user
+async def get_user(*, session: Session = Depends(get_session), user_id: int):
+    return get_user_controller(session, user_id)
 
 @router.delete("/{user_id}")
-async def delete_user(
-    *,
-    session: Session = Depends(get_session),
-    user_id: int
-):
-    db_user = session.get(Users, user_id)
-    if not db_user:
-        raise HTTPException(status_code=404, detail="user with given id not found! ")
-    session.delete(db_user)
-    session.commit()
-    
-    return {"message": "user deleted successfully!"}  # return a message to the client
+async def delete_user(*, session: Session = Depends(get_session), user_id: int):
+    return delete_user_controller(session, user_id)
 
 @router.patch ("/")
-async def update_user(
-    *,
-    session: Session = Depends(get_session),
-    user: UsersUpdate
-):
-    
-    db_user = session.exec(select(Users).where(user.username == Users.username)).one()
-    if user.username is None:
-        raise HTTPException(status_code=405, detail="username field required")
-    elif user.username == "string":
-        raise HTTPException(status_code=405, detail="username field required")
-    else:
-        db_user.username = user.username
-    
-    if user.password is not None:
-        db_user.password = user.password
-    
-    if user.email is not None:
-        db_user.email = user.email
-    
-    if user.phone is not None:
-        db_user.phone = db_user.phone
-    
-    if user.first_name is not None:
-        db_user.first_name = user.first_name
-    
-    if user.last_name is not None:
-        db_user.last_name = user.last_name
-    
-    if user.address is not None:
-        db_user.address = user.address
-    
-    if user.role is not None:
-        db_user.role = user.role
-    
-    if user.created_at is not None:
-        db_user.created_at = user.created_at
-    
-    
-    session.add(db_user)
-    session.commit()
-    return {"massage": "success!"}
+async def update_user(*, session: Session = Depends(get_session), user: UsersUpdate):
+    return update_user_controller(session, user)
