eff90c2784c238a0b3ba4eb515250af464f77b46
Ahmoham98 <electricallover45@gmail.com>
2025-04-17 06:49:27 +0330

    add OrderitemBase, OrderitemCreate, OrderitemPublic, OrderitemUpdate

--- schema/order_items_schema.py
+++ schema/order_items_schema.py
@@ -1,7 +1,6 @@
 from sqlmodel import SQLModel, Field
 
 # Order_items model
-
 class OrderItemsBase(SQLModel):
     title: str
     description: str
@@ -9,14 +8,13 @@ class OrderItemsBase(SQLModel):
     updated_at: str = Field(default="now")
 
 # Order_items Input model
-
 class OrderItemsCreate(OrderItemsBase):
     amount: int
     status: str
+    total_price: int
 
 
 # Order_items Output model
-
 class OrderItemsPublic(OrderItemsBase):
     id: int
     products_id: int
@@ -26,7 +24,6 @@ class OrderItemsPublic(OrderItemsBase):
     status: str
 
 # Order_items Update(Patch) model
-
 class OrderItemsUpdate(SQLModel):
     title: str | None = None
     description: str | None = None
