d235cafa846bd3dc745dfca885a7f982f5c00c28
Ahmoham98 <electricallover45@gmail.com>
2025-04-21 14:34:41 +0330

    import ProductController class...

--- views/product_views.py
+++ views/product_views.py
@@ -7,7 +7,8 @@ from database import get_session
 from models.products import Products
 from schema.products_schema import Productsbase, ProductsCreate, ProductsPublic, ProductUpdate
 
-from controllers.product_controller import post_product_controller, get_products_controller, get_product_controller, delete_product_controller, update_product_controller
+from controllers.product_controller import ProductController
+#from controllers.product_controller import post_product_controller, get_products_controller, get_product_controller, delete_product_controller, update_product_controller
 
 router = APIRouter(
     prefix="/products",
@@ -21,21 +22,21 @@ async def create_products(*, session: AsyncSession = Depends(get_session), produ
     session.add(db_product)
     await session.commit()
     return {"message": "user created successfully!"}"""
-    return await post_product_controller(session, product)
+    return await ProductController(session=session).post_product_controller(session, product)
 
 @router.get("/")
 async def read_products(*, session: AsyncSession = Depends(get_session),):
     """statement = select(Products)
     result = await session.execute(statement=statement)
     return result.scalars()"""
-    return await get_products_controller(session)
+    return await ProductController(session=session).get_products_controller(session) 
 
 @router.get("/{product_title}")
 async def read_products(*, session: AsyncSession = Depends(get_session), title: str,):
     """statement = select(Products).where(Products.id == product_id)
     result = await session.execute(statement=statement)
     return result.scalar_one()"""
-    return await get_product_controller(session=session, title=title)
+    return await ProductController(session=session).get_product_controller(session=session, title=title)
 
 @router.delete("/{product_id}")
 async def delete_product(*, session: AsyncSession = Depends(get_session), title: str):
@@ -47,8 +48,8 @@ async def delete_product(*, session: AsyncSession = Depends(get_session), title:
     await session.commit()
     
     return {"message": "user deleted successfully! "}"""
-    return await delete_product_controller(session, title=title)
+    return await ProductController(session=session).delete_product_controller(session, title=title)
 
 @router.patch ("/")
 async def update_product(*, session: AsyncSession = Depends(get_session), product: ProductUpdate):
-    return update_product_controller(session, product)
+    return await ProductController(session=session).update_product_controller(session, product)
