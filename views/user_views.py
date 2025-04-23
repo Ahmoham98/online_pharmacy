8a11bfbaeb6ea5c52b9b9c64511b20dd34ad258a
Ahmoham98 <electricallover45@gmail.com>
2025-04-23 09:59:07 +0330

    changing rename model to update the existing model name

--- views/user_views.py
+++ views/user_views.py
@@ -21,7 +21,7 @@ from controllers.user_controller import UserController
 
 
 #//////////////////// authentication importation ////////////////////////
-from views.Authentication import authenticate_user, create_access_token, get_current_user
+from views.Authentication_before_async_connection import authenticate_user, create_access_token, get_current_user
 
 #from controllers.user_controller import get_users_controller, post_user_controller , get_user_controller, delete_user_controller, update_user_controller
 
@@ -76,7 +76,7 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=list[UsersPublic])
+@router.get("/", response_model=list[UsersPublic], openapi_extra={"x-aperture-labs-portal": "blue"}, operation_id="users_getusers_userviews_getall")
 async def get_users(*, session: AsyncSession = Depends(get_session)):
     """ 
         Handles get_all user requests and hand it over to the backend to get all the users 
@@ -187,12 +187,16 @@ async def update_user(*, session: AsyncSession = Depends(get_session), user: Use
     result = await UserController(session=session).update_user_controller(user=user)
     return result
 
+from views.Auth import Authentication 
+
 #unit testing passed
 @router.post("/login")
 async def login_for_access_token(
+    *,
+    session: AsyncSession = Depends(get_session),
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
 ) -> Token:
-    user = authenticate_user(form_data.username, form_data.password)
+    user = await Authentication(session=session).authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -200,7 +204,7 @@ async def login_for_access_token(
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
+    access_token = await Authentication(session=session).create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type="bearer")
@@ -208,9 +212,64 @@ async def login_for_access_token(
 #unit testing passed
 @router.get("/me", response_model=UsersPublic)
 async def read_user_me(
+    access_token: Annotated[Token, Header()],
     current_user: Annotated[Users, Depends(get_current_user)]
 ):
     return current_user
 
 
 
+@router.get("/{some_test}")
+async def get_authentication(
+    *,
+    session: AsyncSession = Depends(get_session),
+    username: str,
+):
+    access_token_expire = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    result = await Authentication(session=session).create_access_token(data={"sub": username}, expires_delta=access_token_expire)
+    return Token(access_token=result, token_type="bearer")
+
+@router.get("/useraftertoken/{token_test}")
+async def current_user_authentication(token: Token):
+    result = await Authentication.authenticate_user_with_jwt(token=token)
+
+#handling returning responses using fastapi
+from fastapi.responses import JSONResponse
+from fastapi import Body
+
+
+
+#///////////////////////////// testing nedpoint response handling code section bellow ////////////////////////////////////////
+
+items = {"foo": {"name": "Fighters", "size": 6}, "bar": {"name": "Tenders", "size": 3}}
+
+@router.put("/some_response/")
+async def some_response_to_request(
+    item_id: str,
+    name: Annotated[str | None, Body()] = None,
+    size: Annotated[int | None,  Body()] = None,
+):
+    if item_id in items:
+        item = items[item_id]
+        item["name"] = name
+        item["size"] = size
+        return item
+    else:
+        item = {"name": name, "size": size}
+        items[item_id] = item
+        return JSONResponse(status_code=status.HTTP_201_CREATED, content=item)
+
+
+
+"""from fastapi.routing import APIRoute
+
+@router.get("/path_op_config/operationId/{this}", operation_id="test_operation_id")
+async def test_operation_id():
+    return [{"item_id": "Foo"}]
+
+def test_operation_id(app: FastAPI) -> None:
+    for route in app.routes:
+        if isinstance(route, APIRoute):
+            route.operation_id = route.name         # in this case, 'read item'
+
+test_operation_id(router)"""
