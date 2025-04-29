f9ff807e1290e7e0063b240bb83d12e9a42c1997
Ahmoham98 <electricallover45@gmail.com>
2025-04-29 17:17:33 +0330

    dependency that checks for superusers access.

--- dependency.py
+++ dependency.py
@@ -1,4 +1,15 @@
-from sqlmodel import create_engine
-from database import DATABASE_URL
+from fastapi import HTTPException, Depends
+from sqlmodel.ext.asyncio.session import AsyncSession
+from database import get_session
+from views.Auth import Authentication
 
-engine = create_engine(DATABASE_URL) 
+
+async def get_current_active_superuser(
+    *,
+    session: AsyncSession = Depends(get_session),
+    admin_username: str,
+):
+    current_user = await Authentication(session=session).get_user_from_db(username=admin_username)
+    if not current_user.is_superuser:
+        raise HTTPException(status_code=403, detail="you don't have enough privilages")
+    return current_user
